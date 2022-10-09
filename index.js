7641f16dfbf0d4c0eeee12fe5e3885415c090dec
Dalton Carlile <dlcarlile93@hotmail.com>
2022-10-08 17:30:07 -0700

    commit tree 8a3f6edd2e72f297a393904a90a94ba8dbc85ffc

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bowl`);
+        
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+const cards = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+        console.log(`Thank you, ${cards[i]}, for the wonderful suprise gift`);
+        debugger;
+    }
+    return cards;
+}
+
+writeCards(cards);
+
+writeCards(cards);
